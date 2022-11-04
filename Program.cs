96f4523c9d261fc5912697184092e5b6641b678c
HoussamTachnout <houssam.tachnout@usmba.ac.ma>
2022-11-04 20:53:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice3
+{
+    class Point
+    {
+        private int X, Y;
+        public Point(int X,int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+        public int GetX()
+        {
+            return X;
+        }
+        public void SetX(int X)
+        {
+            this.X = X;
+        }
+        public int GetY()
+        {
+            return Y;
+        }
+        public void SetY(int Y)
+        {
+            this.Y = Y;
+        }
+        public double Distance(Point P)
+        {
+            return Math.Sqrt(Math.Pow((P.GetX() - this.GetX()), 2) + Math.Pow((P.GetY() - this.GetY()), 2));
+        }
+        public int Egalite(Point P)
+        {
+            if ((P.X == X) && (P.Y == Y))
+                return 1;
+            else
+                return 0;
+        }
+
+    }
+}
